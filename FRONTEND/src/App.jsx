f62e32b1660b785a41d2979922373ad512256f3b
Anandam Chattrapate <23eg105e05@anurag.edu.in>
2026-02-22 23:48:56 +0530

    Update App.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,18 +1,17 @@
 // App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; // Add Navigate
 import { useEffect } from 'react';
 import Login from './pages/Login.jsx';
 import ChatPage from './pages/ChatPage.jsx';
 import Register from './pages/Register.jsx';
-// Import socket functions
-//import { initializeSocket, disconnectSocket } from './services/socket.js';
 
 function App() {
- 
-
   return (
     <BrowserRouter>
       <Routes>
+        {/* Add this line to redirect root to login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        
         <Route path='/login' element={<Login />} />
         <Route path='/chat' element={<ChatPage />} />
         <Route path='/register' element={<Register />} />
